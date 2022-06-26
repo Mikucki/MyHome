802e22ea3d6bdb59f68617ecfca2186a9b5ebba3
Mikucki <piotr.mikucki@op.pl>
2022-06-26 16:38:06 -0400

    added added styling to text

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
-const select = document.querySelector('select');
-const list = document.querySelector('ul');
-const h1 = document.querySelector('h1');
+const grade = prompt("")
 
-select.addEventListener('change', () => {
-  const choice = select.value;
 
-  let days = 31;
-  if (choice === 'February') {
-    days = 28;
-  } else if (choice === 'April' || choice === 'June' || choice === 'September'|| choice === 'November') {
-    days = 30;
-  }
-
-  createCalendar(days, choice);
-});
-
-function createCalendar(days, choice) {
-  list.innerHTML = '';
-  h1.textContent = choice;
-  for (let i = 1; i <= days; i++) {
-    const listItem = document.createElement('li');
-    listItem.textContent = i;
-    list.appendChild(listItem);
-  }
+switch (true) {
+    case grade >= 90:
+        console.log("A");
+        brake;
+    case grade >= 80:
+        console.log("B");
+        brake;
+    case grade >= 70:
+        console.log("C");
+        brake;
+    case grade >= 60:
+        console.log("D");
+        brake;
+    default:
+        console.log("FFFFF")
 }
 
-createCalendar(31,'January');
