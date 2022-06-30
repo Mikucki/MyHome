7e75c177ab82b49c8a674db219a842e619dfa567
Mikucki <piotr.mikucki@op.pl>
2022-06-30 16:11:10 -0400

    added main.js

--- main.js
+++ main.js
@@ -1,20 +1,2 @@
-const grade = prompt("")
 
-
-switch (true) {
-    case grade >= 90:
-        console.log("A");
-        brake;
-    case grade >= 80:
-        console.log("B");
-        brake;
-    case grade >= 70:
-        console.log("C");
-        brake;
-    case grade >= 60:
-        console.log("D");
-        brake;
-    default:
-        console.log("FFFFF")
-}
-
+  //learning dom model//
